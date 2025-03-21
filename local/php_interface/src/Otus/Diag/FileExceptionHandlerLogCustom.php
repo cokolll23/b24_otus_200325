220554784172e2a220246b65da9356b675be3c82
ALEX <cavjob@ya.ru>
2025-03-21 13:36:07 +0300

    Otus Отладка и логирование ДЗ

---
+++ local/php_interface/src/Otus/Diag/FileExceptionHandlerLogCustom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Otus\Diag;
+
+class FileExceptionHandlerLogCustom
+{
+    public static function print()
+    {
+        echo 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+<?php \Otus\Diag\FileExceptionHandlerLogCustom::print(); ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
