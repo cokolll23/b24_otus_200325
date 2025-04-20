5e34f98d698ac57e867ff583867abf1ba65916fe
ALEX <cavjob@ya.ru>
2025-04-21 01:01:07 +0300

    Создать php файл /otus/debug.php. В нем написать код, который, при обращении к нему по HTTP, будет записывать в файл текущие дату и время

--- otus/debug.php
+++ otus/debug.php
@@ -1,7 +1,8 @@
-<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
 <?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+
 $currentDate = new DateTime();
-echo $curDate =$currentDate->format('Y-m-d H:i:s');
-$var = [];
-Bitrix\Main\Diag\Debug::writeToFile($curDate, $varName = 'curDateTime', $fileName = '/log.txt');
-file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logs/log.txt', $curDate . PHP_EOL, FILE_APPEND);
+$curDate = $currentDate->format('Y-m-d H:i:s');
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/otus/logs.txt', $curDate . PHP_EOL, FILE_APPEND);
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
