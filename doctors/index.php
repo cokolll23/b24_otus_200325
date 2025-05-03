36b9cfe2f292e4869da93b17e015abeb76931109
ALEX <cavjob@ya.ru>
2025-05-03 17:50:31 +0300

    Вывод /doctors/ Врачей и переход в детальную карточку

--- doctors/index.php
+++ doctors/index.php
@@ -1,6 +1,9 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-Bitrix\Main\Page\Asset::getInstance()->addCss('/doctors/styles.css');
+$curPage = $APPLICATION->GetCurPage();
+Bitrix\Main\Page\Asset::getInstance()->addCss($curPage . '/styles.css');
+
+use Otus\Doctors\Models\Lists\DoctorsPropertyValuesTable;
 
 if (!$_GET['id']) {
     $APPLICATION->SetPageProperty("keywords", "Врачи сайт компания");
@@ -9,47 +12,41 @@ if (!$_GET['id']) {
     $APPLICATION->SetTitle("Врачи");
 
 
-    $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
+    $doctors = DoctorsPropertyValuesTable::getList([
         'select' => [
-            'ID',
-            'NAME',
+            'IBLOCK_ID' => 'ELEMENT.IBLOCK_ID',
+            'ELEMENT_ID' => 'IBLOCK_ELEMENT_ID',
+            'LAST_NAME' => 'ELEMENT.NAME',
             'FIRST_NAME_' => 'FIRST_NAME',
             'MIDDLE_NAME_' => 'MIDDLE_NAME',
-            'PROCEDURE_NAME' => 'PROCEDURES.ELEMENT.NAME'
         ],
-        'filter' => [// 'ID' => $doctorId
-        ]
+
     ])->fetchAll();
-    foreach ($doctors as $doctor) {
-        $arDoctors[$doctor['ID']]['DOCTOR_LAST_NAME'] = $doctor['NAME'];
-        $arDoctors[$doctor['ID']]['DOCTOR_FIRST_NAME'] = $doctor['FIRST_NAME_VALUE'];
-        $arDoctors[$doctor['ID']]['DOCTOR_MIDDLE_NAME'] = $doctor['MIDDLE_NAME_VALUE'];
-        $arDoctors[$doctor['ID']]['DOCTOR_PROCEDERES_NAME'][] = $doctor['PROCEDURE_NAME'];
-    }
     ?>
-
-
     <div class="doctors-block cards-list">
         <?php
-        foreach ($arDoctors as $i => $doctor) { ?>
-            <a href="/doctors/?id=<?= $i; ?>" id="id_<?= $i; ?>" class="card">
-                <?= $doctor['DOCTOR_LAST_NAME'] . ' ' . $doctor['DOCTOR_FIRST_NAME'] . ' ' . $doctor['DOCTOR_MIDDLE_NAME'] ?>
+        foreach ($doctors as $i => $doctor) { ?>
+            <a href="<?= $curPage ?>?id=<?= $doctor['ELEMENT_ID']; ?>" id="id_<?= $doctor['ELEMENT_ID']; ?>"
+               class="card">
+                <?= $doctor['LAST_NAME'] . ' ' . $doctor['FIRST_NAME'] . ' ' . $doctor['MIDDLE_NAME'] ?>
             </a>
         <?php } ?>
     </div>
-<?php } else {?>
-<a href="/doctors">К списку врачей</a>
+<?php } else {
 
-   <?php $doctorId = $_GET['id'];
-
-    $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
+    if ($_GET['id']) {
+        $doctorId = $_GET['id'];
+    } ?>
+    <div>
+        <a href="<?= $curPage; ?>">К списку врачей</a>
+    </div>
+    <?php $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
         'select' => [
             'ID',
             'NAME',
             'FIRST_NAME_' => 'FIRST_NAME',
             'MIDDLE_NAME_' => 'MIDDLE_NAME',
             'PROCEDURE_NAME' => 'PROCEDURES.ELEMENT.NAME',
-            //'PROCEDURE_'=>'PROCEDURES.ELEMENT',// вывод всех полей свойства
         ],
         'filter' => [
             'ID' => $doctorId
@@ -62,23 +59,19 @@ if (!$_GET['id']) {
         $arDoctors[$doctor['ID']]['DOCTOR_PROCEDERES_NAME'][] = $doctor['PROCEDURE_NAME'];
     }
     $fio = $arDoctors[$doctorId]['DOCTOR_LAST_NAME'] . ' ' . $arDoctors[$doctorId]['DOCTOR_FIRST_NAME'] . ' ' . $arDoctors[$doctorId]['DOCTOR_MIDDLE_NAME'];
-
-
     $APPLICATION->SetPageProperty("title", $fio);
-    $APPLICATION->SetTitle($fio); ?>
-    <div class="doc-container">
-    <h2>
-        <?= $fio; ?>
-    </h2>
-    <h3>
+    $APPLICATION->SetTitle($fio);
+    echo '<div class="doctor-block">';
+    echo '<h3 class="doctor-name">';
+    echo $fio;
+    echo '</h3>';
+    ?>
     Процедуры :
-    </h3>
     <ul>
         <?php foreach ($arDoctors[$doctorId]['DOCTOR_PROCEDERES_NAME'] as $DOCTOR_PROCEDERE_NAME): ?>
             <li><?= $DOCTOR_PROCEDERE_NAME ?></li>
         <?php endforeach; ?>
     </ul>
-<?php }
-?>
     </div>
+<?php } ?>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- doctorss/index.php
+++ doctorss/index.php
@@ -1,7 +1,9 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$curPage = $APPLICATION->GetCurPage();
+Bitrix\Main\Page\Asset::getInstance()->addCss($curPage . '/styles.css');
 
-Bitrix\Main\Page\Asset::getInstance()->addCss('/doctors/styles.css');
+use Otus\Doctors\Models\Lists\DoctorsPropertyValuesTable;
 
 if (!$_GET['id']) {
     $APPLICATION->SetPageProperty("keywords", "Врачи сайт компания");
@@ -10,48 +12,42 @@ if (!$_GET['id']) {
     $APPLICATION->SetTitle("Врачи");
 
 
-    $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
+    $doctors = DoctorsPropertyValuesTable::getList([
         'select' => [
-            'ID',
-            'NAME',
+            'IBLOCK_ID' => 'ELEMENT.IBLOCK_ID',
+            'ELEMENT_ID' => 'IBLOCK_ELEMENT_ID',
+            'LAST_NAME' => 'ELEMENT.NAME',
             'FIRST_NAME_' => 'FIRST_NAME',
             'MIDDLE_NAME_' => 'MIDDLE_NAME',
-            'PROCEDURE_NAME' => 'PROCEDURES.ELEMENT.NAME',
-            //'PROCEDURE_'=>'PROCEDURES.ELEMENT',// вывод всех полей свойства
         ],
-        'filter' => [// 'ID' => $doctorId
-        ]
+
     ])->fetchAll();
-    foreach ($doctors as $doctor) {
-        $arDoctors[$doctor['ID']]['DOCTOR_LAST_NAME'] = $doctor['NAME'];
-        $arDoctors[$doctor['ID']]['DOCTOR_FIRST_NAME'] = $doctor['FIRST_NAME_VALUE'];
-        $arDoctors[$doctor['ID']]['DOCTOR_MIDDLE_NAME'] = $doctor['MIDDLE_NAME_VALUE'];
-        $arDoctors[$doctor['ID']]['DOCTOR_PROCEDERES_NAME'][] = $doctor['PROCEDURE_NAME'];
-    }
+    pretty_print($doctors);
     ?>
-
-
     <div class="doctors-block cards-list">
         <?php
-        foreach ($arDoctors as $i => $doctor) { ?>
-            <a href="/doctors/?id=<?= $i; ?>" id="id_<?= $i; ?>" class="card">
-                <?= $doctor['DOCTOR_LAST_NAME'] . ' ' . $doctor['DOCTOR_FIRST_NAME'] . ' ' . $doctor['DOCTOR_MIDDLE_NAME'] ?>
+        foreach ($doctors as $i => $doctor) { ?>
+            <a href="<?= $curPage ?>/?id=<?= $doctor['ELEMENT_ID']; ?>" id="id_<?= $doctor['ELEMENT_ID']; ?>"
+               class="card">
+                <?= $doctor['LAST_NAME'] . ' ' . $doctor['FIRST_NAME'] . ' ' . $doctor['MIDDLE_NAME'] ?>
             </a>
         <?php } ?>
     </div>
 <?php } else {
 
-
-    $doctorId = $_GET['id'];
-
-    $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
+    if ($_GET['id']) {
+        $doctorId = $_GET['id'];
+    } ?>
+    <div>
+        <a href="<?= $curPage; ?>">К списку врачей</a>
+    </div>
+    <?php $doctors = \Bitrix\Iblock\Elements\ElementDoctorsTable::getList([ //быстрая выборка ORM getList необходимо обозначить Символьный код и Символьный код API здесь doctors
         'select' => [
             'ID',
             'NAME',
             'FIRST_NAME_' => 'FIRST_NAME',
             'MIDDLE_NAME_' => 'MIDDLE_NAME',
             'PROCEDURE_NAME' => 'PROCEDURES.ELEMENT.NAME',
-            //'PROCEDURE_'=>'PROCEDURES.ELEMENT',// вывод всех полей свойства
         ],
         'filter' => [
             'ID' => $doctorId
@@ -64,17 +60,17 @@ if (!$_GET['id']) {
         $arDoctors[$doctor['ID']]['DOCTOR_PROCEDERES_NAME'][] = $doctor['PROCEDURE_NAME'];
     }
     $fio = $arDoctors[$doctorId]['DOCTOR_LAST_NAME'] . ' ' . $arDoctors[$doctorId]['DOCTOR_FIRST_NAME'] . ' ' . $arDoctors[$doctorId]['DOCTOR_MIDDLE_NAME'];
-
-
     $APPLICATION->SetPageProperty("title", $fio);
     $APPLICATION->SetTitle($fio);
-    echo $fio
+    echo '<h4 class="doctor-block">';
+    echo $fio;
+    echo '</h4>';
     ?>
+        Процедуры :
     <ul>
         <?php foreach ($arDoctors[$doctorId]['DOCTOR_PROCEDERES_NAME'] as $DOCTOR_PROCEDERE_NAME): ?>
             <li><?= $DOCTOR_PROCEDERE_NAME ?></li>
         <?php endforeach; ?>
     </ul>
-<?php }
-?>
+<?php } ?>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
